c0daa6dd3af0baaec4dffdb94a0eec1d86e00a2e
Slavi Dimitrov <slavi@orbisds.com>
2024-07-18 16:27:23 +0300

    Add responsible images for all tables

--- src/components/Footer/Modals/Tables/Tables.jsx
+++ src/components/Footer/Modals/Tables/Tables.jsx
@@ -19,32 +19,55 @@ export default function Tables() {
                 <div className="col-lg-8">
                   <div className="modal-body">
                     {/* Project details*/}
-                    <h2 className="text-uppercase">Project Name</h2>
+                    <h2 className="text-uppercase">,,Червеният дявол"</h2>
                     <p className="item-intro text-muted">
-                      Lorem ipsum dolor sit amet consectetur.
+                      Е перфектната маса за Вашия дом!
                     </p>
                     <img
-                      className="img-fluid d-block mx-auto"
-                      src="assets/img/portfolio/1.jpg"
-                      alt="..."
+                      className="top-bottom"
+                      src="assets/img/portfolio/ten/1.jpg"
+                      alt="red devil table"
+                    />
+                    <div className="image-container">
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/ten/2.jpg"
+                        alt="red devil table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/ten/4.jpg"
+                        alt="red devil table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/ten/5.jpg"
+                        alt="red devil table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/ten/6.jpg"
+                        alt="red devil table"
+                      />
+                    </div>
+                    <img
+                      className="top-bottom"
+                      src="assets/img/portfolio/ten/3.jpg"
+                      alt="red devil table"
                     />
                     <p>Това място ще се използва за описание на проекта. </p>
                     <ul className="list-inline">
                       <li>
                         <strong>Клиент:</strong>
-                        #Човек
+                        Търси се...
                       </li>
                       <li>
                         <strong>Категория:</strong>
-                        Къщи
+                        Маси с епоксидна смола
                       </li>
                       <li>
-                        <strong>Квадратура:</strong>
-                        #?
-                      </li>
-                      <li>
-                        <strong>Изолация:</strong>
-                        #?
+                        <strong>Състояние:</strong>
+                        <p id="selling">Продава сe</p>
                       </li>
                     </ul>
                     <button
@@ -53,7 +76,7 @@ export default function Tables() {
                       type="button"
                     >
                       <i className="fas fa-xmark me-1" />
-                      Close Project
+                      Затвори проекта
                     </button>
                   </div>
                 </div>
@@ -80,15 +103,62 @@ export default function Tables() {
                 <div className="col-lg-8">
                   <div className="modal-body">
                     {/* Project details*/}
-                    <h2 className="text-uppercase">Project Name</h2>
+                    <h2 className="text-uppercase">Синя смола</h2>
                     <p className="item-intro text-muted">
-                      Lorem ipsum dolor sit amet consectetur.
+                      В перфектен синхрон с дървото
                     </p>
-                    <img
-                      className="img-fluid d-block mx-auto"
-                      src="assets/img/portfolio/1.jpg"
-                      alt="..."
-                    />
+                    <div className="image-container">
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/1.png"
+                        alt="Blue epoxy resin table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/w2.jpg"
+                        alt="Blue epoxy resin table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/w3.jpg"
+                        alt="Blue epoxy resin table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/w4.jpg"
+                        alt="Blue epoxy resin table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/h1.jpg"
+                        alt="Blue epoxy resin table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/h2.jpg"
+                        alt="Blue epoxy resin table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/h3.jpg"
+                        alt="Blue epoxy resin table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/h4.jpg"
+                        alt="Blue epoxy resin table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/h5.jpg"
+                        alt="Blue epoxy resin table"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/eleven/h6.jpg"
+                        alt="Blue epoxy resin table"
+                      />
+                    </div>
                     <p>Това място ще се използва за описание на проекта. </p>
                     <ul className="list-inline">
                       <li>
@@ -114,7 +184,7 @@ export default function Tables() {
                       type="button"
                     >
                       <i className="fas fa-xmark me-1" />
-                      Close Project
+                      Затвори проекта
                     </button>
                   </div>
                 </div>
@@ -141,32 +211,38 @@ export default function Tables() {
                 <div className="col-lg-8">
                   <div className="modal-body">
                     {/* Project details*/}
-                    <h2 className="text-uppercase">Project Name</h2>
-                    <p className="item-intro text-muted">
-                      Lorem ipsum dolor sit amet consectetur.
-                    </p>
-                    <img
-                      className="img-fluid d-block mx-auto"
-                      src="assets/img/portfolio/1.jpg"
-                      alt="..."
-                    />
+                    <h2 className="text-uppercase">Маса с фосфор</h2>
+                    <p className="item-intro text-muted">Светеща в тъмното</p>
+                    <div className="image-container">
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/twelve/2.jpg"
+                        alt="table that glows in the dark"
+                      />
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/twelve/1.jpg"
+                        alt="table that glows in the dark"
+                      />
+
+                      <img
+                        className="slide"
+                        src="assets/img/portfolio/twelve/3.png"
+                        alt="table that glows in the dark"
+                      />
+                    </div>
                     <p>Това място ще се използва за описание на проекта. </p>
                     <ul className="list-inline">
                       <li>
-                        <strong>Клиент:</strong>
-                        #Човек
+                        <strong>Клиент:</strong>Анонимен
                       </li>
                       <li>
                         <strong>Категория:</strong>
-                        Къщи
+                        Маси с епоксидна смола
                       </li>
                       <li>
-                        <strong>Квадратура:</strong>
-                        #?
-                      </li>
-                      <li>
-                        <strong>Изолация:</strong>
-                        #?
+                        <strong>Качества:</strong>
+                        Издръжливост, естетика, светеща в тъмното
                       </li>
                     </ul>
                     <button
@@ -175,7 +251,7 @@ export default function Tables() {
                       type="button"
                     >
                       <i className="fas fa-xmark me-1" />
-                      Close Project
+                      Затвори проекта
                     </button>
                   </div>
                 </div>
